32ecc67376f9cc2227dac360895bd805e110a523
Yusha D <jojomemes@yandex.ru>
2024-03-17 21:01:27 +0400

    Start hw5bonus

--- scrapper/src/main/java/edu/java/client/bot/dto/request/LinkUpdate.java
+++ scrapper/src/main/java/edu/java/client/bot/dto/request/LinkUpdate.java
@@ -3,12 +3,15 @@ package edu.java.client.bot.dto.request;
 import jakarta.validation.constraints.NotEmpty;
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 
 public record LinkUpdate(
     @NotNull Long id,
     @NotNull URL link,
     @NotEmpty String description,
-    @NotEmpty List<Long> tgChatIds
+    @NotEmpty List<Long> tgChatIds,
+
+    @NotEmpty Map<String, String> metaInfo
 ) {
 }
